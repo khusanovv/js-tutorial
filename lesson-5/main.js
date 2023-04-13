9f9394b15e4adcc5ad5f7f9144bb21547064579b
Boburshoh Khusanov <110517357+khusanovv@users.noreply.github.com>
2023-04-13 16:44:40 +0500

    While Do while, Function

---
+++ lesson-5/main.js
@@ -0,0 +1,41 @@
+// while (1) {
+//   console.log(`hey`);
+// }
+
+// #Teskasi sanoq
+
+// let i = 3
+// while(i){
+//     alert(i)
+//     i--
+// }
+
+// ## Function decloration
+
+// getArv()
+
+// function getArv() {
+//   console.log("Hello Khusanov Welcome to Cambiridge");
+// }
+
+// ###Function experssion
+
+// const getArv = function(){
+//     console.log("Hello Khusanov Welcome to Cambiridge");
+// }
+// getArv()
+
+// ####Arrov fuction
+
+// const getArv = () =>{
+//     console.log("Hello Khusanov Welcome to Cambiridge");
+// }
+
+// ####Callback fuction
+
+// const getArv = function (name, surname, age) {
+//   console.log(`Name ${name} `);
+//   console.log(`Surname ${surname}`);
+//   console.log(`Age ${age} `);
+// };
+// getArv("Boburshoh", "Khusanov ", 20);
